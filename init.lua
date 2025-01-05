d44b0735706ac420890af7822c14f08821a74d78
miku <shalshyt@163.com>
2025-01-06 00:09:16 +0800

    解决bug，先加载lazy.nvim插件，保证options.lua中的主题能正常加载

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
+require("plugins.plugins-setup")
+
 require("core.options")
 require("core.keymaps")
 
 -- 插件
-require("plugins.plugins-setup")
 require("plugins.lualine")
 require("plugins/nvim-tree")
 require("plugins/treesitter")
