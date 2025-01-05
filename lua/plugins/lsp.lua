84e6ef3948a8d7d8cddc21cbb94bcdf88879b6d2
miku <shalshyt@163.com>
2025-01-06 00:32:50 +0800

    增加c++lsp clangd

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,11 +12,21 @@ require("mason-lspconfig").setup {
     -- ensure_installed = { "lua_ls", "rust_analyzer" },
     ensure_installed = {
       "lua_ls",
+      "clangd",
     }
 }
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require("lspconfig").lua_ls.setup {
+local lspconfig = require('lspconfig')
+
+lspconfig.lua_ls.setup {
   capabilities = capabilities,
 }
+
+lspconfig.clangd.setup({
+  cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
+  init_options = {
+    fallbackFlags = { '-std=c++17' },
+  },
+})
